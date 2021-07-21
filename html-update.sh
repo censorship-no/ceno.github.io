ce7798ef4aa3ca64574caa17dc6371fd3917a459
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2021-07-21 11:11:40 +0200

    Move all CSS into LTR or RTL-specific CSS files.
    Then only either file is used in the HTML page.

--- html-update.sh
+++ html-update.sh
@@ -11,9 +11,6 @@ cd "$1"
 
 if echo "$RTL_LANGUAGES" | grep -qE "\b$1\b"; then
     dir=rtl
-
-    # TODO: construct argument list for sed
-    rtl_css="$(find ../css -name '*rtl*' -exec basename '{}' ';')"
 else
     dir=ltr
 fi
@@ -23,11 +20,9 @@ for po in *.po; do
     po2html --progress=none -i "$po" -t "../en/$html" -o "$html"
 
     if [ $dir = rtl ]; then
-        sed -i 's/\(<html .* dir="\)ltr\(".*\)/\1rtl\2/' "$html"
-
-        for css_rtl in $rtl_css; do
-            css_ltr="$(echo "$css_rtl" | sed 's/.rtl//')"
-            sed -i "s#/$css_ltr\"#/$css_rtl\"#g" "$html"
-        done
+        sed -Ei \
+            -e 's#(<html\b.*\bdir=")ltr(")#\1rtl\2#' \
+            -e 's#(<link\b.*\bhref="[^"]+/ceno-)ltr(\.css")#\1rtl\2#' \
+            "$html"
     fi
 done
